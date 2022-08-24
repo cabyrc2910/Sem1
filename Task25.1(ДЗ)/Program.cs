8f9ac96de3dd0050942991f3310ad9cfd1033d40
cabyrc2910 <cabyrc@list.ru>
2022-08-24 22:00:24 +0300

    Сохранение

---
+++ Task25.1(ДЗ)/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+// Пример 3,5->243(3⁵)  2,4->16  Не использовать MathPow  и аналоги
+
+Console.Clear();
+Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int C = 1;
+while ( count < B )
+{
+    C  = C*A;
+    count = count + 1; 
+}
+Console.WriteLine(C);
--- Task38(ДЗ)/Program.cs
+++ Task38(ДЗ)/Program.cs
@@ -2,32 +2,16 @@
 // Пример  [3 7 22 2 78]->76
 
 Console.Clear();
-Console.WriteLine("Введите колличество элементов");
-int i = Convert.ToInt32(Console.ReadLine());
-double [] array = new double [i];
-int count = 1;
-double max = -100;
-double min = 100;
-while (count <= i )
+Console.WriteLine("Введите кол-во ячеек массива");
+int[] Arr = new int[Convert.ToInt32(Console.ReadLine())];
+for (int i = 0 ; i<Arr.Length ; i++)
 {
-    Random x = new Random();
-    double n = x.Next(-100,100);
-    array[count -1]= n;
-    if (n > max)
-    {
-     max = n;
-    }
-    if (n < min)
-    {
-     min = n;
-    }
-    Console.Write(n);
-    Console.Write(" ");
-    count++;
+    Console.WriteLine("Введите значение ячейки массива под номером " + (i+1));
+    Arr[i] = Convert.ToInt32(Console.ReadLine());
 }
-Console.Write("max =");
-Console.WriteLine(max);
-Console.Write("min =");
-Console.WriteLine(min);
-Console.Write("разница =");
+int max = Arr.Max();
+int min = Arr.Min();
+Console.WriteLine("Максимальное значение в массиве " + max);
+Console.WriteLine("Минимальное значение в массиве " + min);
+Console.Write("разница = ");
 Console.WriteLine(max - min);
